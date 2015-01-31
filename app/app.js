3c7dbe0302525e67b9081ce92dbb7c1029d1c0a7
unknown <jan.castor14@gmail.com>
2015-01-31 12:42:02 -0500

    Updated app.js. Removed interval. Removed socket.io dependency.

--- app/app.js
+++ app/app.js
@@ -11,19 +11,17 @@ var myAnalogPin = new mraa.Aio(0);
  */
 function startSensorWatch(res) {
     'use strict';
-    setInterval(function () {
-        var a = myAnalogPin.read();
-        console.log("Analog Pin (A0) Output: " + a);
-        //console.log("Checking....");
+    var a = myAnalogPin.read();
+    console.log("Analog Pin (A0) Output: " + a);
+    //console.log("Checking....");
 
-        var resistance = (1023 - a) * 10000 / a; //get the resistance of the sensor;
-        //console.log("Resistance: "+resistance);
-        var celsius_temperature = 1 / (Math.log(resistance / 10000) / B + 1 / 298.15) - 273.15;//convert to temperature via datasheet ;
-        //console.log("Celsius Temperature "+celsius_temperature);
-        var fahrenheit_temperature = (celsius_temperature * (9 / 5)) + 32;
-        console.log("Fahrenheit Temperature: " + fahrenheit_temperature);
-        res.json({ message: 'temperature get: ' + fahrenheit_temperature });
-    }, 4000);
+    var resistance = (1023 - a) * 10000 / a; //get the resistance of the sensor;
+    //console.log("Resistance: "+resistance);
+    var celsius_temperature = 1 / (Math.log(resistance / 10000) / B + 1 / 298.15) - 273.15;//convert to temperature via datasheet ;
+    //console.log("Celsius Temperature "+celsius_temperature);
+    var fahrenheit_temperature = (celsius_temperature * (9 / 5)) + 32;
+    console.log("Fahrenheit Temperature: " + fahrenheit_temperature);
+    res.json({ message: 'temperature get: ' + fahrenheit_temperature });
 }
 
 console.log("Sample Reading Grove Kit Temperature Sensor");
